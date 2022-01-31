9521b6c3e66f81a635ea137675fcb55f0b512c97
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2022-01-31 10:38:16 +0100

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Link from 'next/link';
+function Header() {
+    return (
+        <header className='flex justify-between p-5 max-w-8xl mx-auto bg-blue-200'>
+            <div className='flex items-center space-x-5'>
+                <Link href='/'>
+                    <img
+                        className='w-44 object-contain cursor-pointer'
+                        src='https://links.papareact.com/yvf'
+                    />
+                </Link>
+
+                <div className='hidden md:inline-flex items-center space-x-5'>
+                    {/*<h3>*/}
+                    {/*    Our Story*/}
+                    {/*</h3>*/}
+
+                    {/*<h3>*/}
+                    {/*    Membership*/}
+                    {/*</h3>*/}
+
+                    {/*<h3>*/}
+                    {/*    Write*/}
+                    {/*</h3>*/}
+
+                    {/*<h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>*/}
+                    {/*    Follow*/}
+                    {/*</h3>*/}
+                </div>
+            </div>
+
+            <div className='flex items-center space-x-5 text-black-600'>
+                <h3>
+                    Our Story
+                </h3>
+
+                <h3>
+                    Membership
+                </h3>
+
+                <h3>
+                    Write
+                </h3>
+
+                <h3>
+                    Sign In
+                </h3>
+
+                <h3 className='text-white bg-green-500 px-4 py-1 rounded-full'>
+                    Get Started
+                </h3>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,45 @@
 import Head from 'next/head'
+import Header from '../components/Header';
+import React from "react";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className='max-w-8xl mx-auto'>
+            <Head>
+                <title>
+                    Medior
+                </title>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
+            <Header />
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            <div className='flex justify-between items-center bg-blue-200 border-y border-black py-10 lg:py-0'>
+                <div className='px-20 space-y-4'>
+                    <h1 className='text-6xl max-w-xl font-serif'>
+                        <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect
+                    </h1>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+                    <h2>
+                        It's easy and free to post your thinking on any topic and connect with millions of readers.
+                    </h2>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+                    <div className='flex items-center pb-5'>
+                        <h3 className='text-white bg-green-500 px-4 py-1 rounded-full'>
+                            Start Writing
+                        </h3>
+                    </div>
+                </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <div className=''>
+                    <img
+                        className='hidden md:inline-flex h-32 lg:h-full'
+                        src='https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-medium-1.png'
+                        alt='Medium'
+                    />
+                </div>
+            </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
